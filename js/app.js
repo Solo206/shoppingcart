c9de50f3ead3a0a1d4f96ff060b42bf54f77a53b
solo206 <jsolk206@gmail.com>
2014-08-01 12:46:27 -0700

    next one

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
-$(document).ready(function{
-	// var entry=$('.inputbox').val();
-	// $('.add').mousedown( function(){
-	// 	$('button').css('background':'green');
-	// });
-	$('.inputbox').keydown( function(){
-		alert("handler called");
+$(document).ready(function(){
+	
+	$('.inputbox').click(function(){
+		$('body').css({'background':'red'});
+		$('.inputbox').val("");
+
 	});
+	$('.subtitle').find('.add').on('click', function(){
+		// console.log('clicked');
+		$('body').css({'background':'green'});
+		addItem();
+	});
+	$('.subtitle .add').on('keydown', '.inputbox', function(event){
+		if(event.which==13){
+			addItem();
+		}
+	});
+	function addItem(){
+		$('.itemlist').prepend('<li class="item">'+$('.inputbox').val()+'</li>');
+		$('.content').css({'height':'+50px'});
+		$('.inputbox').val("");
+	}
 });
