60f018bffd36e962f870dd4f49c8d0791ea0d71a
solo206 <jsolk206@gmail.com>
2014-08-03 19:23:41 -0700

    basic functions completed

--- js/app.js
+++ js/app.js
@@ -1,36 +1,63 @@
 $(document).ready(function(){
 	
-	$('.inputbox').click(function(){
-		$('body').css({'background':'red'});
-		$('.inputbox').val("");
+	// $('.inputbox').click(function(){
+	// 	$('body').css({'background':'red'});
+	// 	$('.inputbox').val("");
 
-	});
+	// });
+	//adding an item on click
 	$('.subtitle').find('.add').on('click', function(){
 		// console.log('clicked');
-		$('body').css({'background':'green'});
 		addItem();
 	});
-	$('.subtitle').on('keyup', '.inputbox', function(event){
-		if(event.which==13){
+
+	//adding an item on enter
+	$('.inputbox').keydown( function(event){
+		var keyvalue=13;
+		if(event.keyCode==keyvalue){
+			event.preventDefault();
 			addItem();
 		}
 	});
-	$('.itemlist').find('.item').on('click', function(){
-		$('body').css({'background':'silver'});
-		$('.item').toggleClass('highlight');
+
+//	highlight item on click and strikethrough text
+	$("#itemList").on('click','.item',function(){
+		// $(this).css({'background':'yellow'});
+		$(this).toggleClass('highlight');
+		// console.log("is this working?")
 	});
 	// $('.item').on('dblclick'), function(){}
+
+	//removing an item on click
 	$('.subtitle').find('.remove').on('click',function(){
-		$('body').css({'background':'yellow'});
 		removeItem();
 	});
-	function addItem(){
-		$('.itemlist').prepend('<li class="item">'+$('.inputbox').val()+'</li>');
-		$('.inputbox').val("");
-	}
-	function removeItem(){
-		if ('.highlight'){
-			$(this).remove();
-		}
-	}
+	$('.instructpush').click(function(){
+		toggleItem();
+	});
+
 });
+	//function which checks if string is empty
+	function isEmpty(value){
+		return value.length===0;
+	}
+	//function which adds item
+	function addItem(){
+		var itemword=$('.inputbox').val();
+		
+		if(!isEmpty(itemword)){
+		var stackitem='<li class="item">'+itemword+'</li>'
+		$('#itemList').prepend(stackitem);
+		}
+		$(this).addClass("item");
+		$('.inputbox').val("");
+		$('.inputbox').focus();
+	}
+	//removes highlighted element
+	function removeItem(){
+		$('.highlight').remove();
+	}
+	//hides and shows item by adding and removing hide class
+	function toggleItem(){
+		$('.instructions').toggleClass('hide')
+	}
